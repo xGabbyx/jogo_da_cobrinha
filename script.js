830dbedb475058c42239a2d563298d53d274728d
xGabbyx <joaogabriel4175@gmail.com>
2022-05-20 00:26:21 -0300

    Criação da comida

--- script.js
+++ script.js
@@ -8,21 +8,31 @@ let snake = []; // CRIA A ARRAY RESPONSÁVEL PELA COBRINHA.
 let direction = "right"; // DEFINE A DIREÇÃO INICIAL DA COBRINHA.
 //------------------------------------------------------------------------------------//
     snake[0] = { x: 8 * box, y: 8 * box }; // DEFINE O TAMANHO DA COBRINHA.
+    let food = { // CRIA A ARRAY RESPONSÁVEL PELA COMIDA.
+        x: Math.floor(Math.random() * 15 + 1) * box, 
+        y: Math.floor(Math.random() * 15 + 1) * box
+    }; 
 //------------------------------------------------------------------------------------//
 //                               ESTRUTURA DO SCRIPT                                  //
 //------------------------------------------------------------------------------------//
-    // CRIA O BACKGROUND
+    // CRIA O BACKGROUND.
     function criarBG() {
         context.fillStyle = "lightgreen";
         context.fillRect(0, 0, 16 * box, 16 * box);
     }
-    // CRIA O QUADRADINHO REFERENTE A COBRINHA
+    // CRIA O QUADRADINHO REFERENTE A COBRINHA.
     function criarCobrinha() {
         for (i = 0; i < snake.length; i++) {
             context.fillStyle = "green";
             context.fillRect(snake[i].x, snake[i].y, box, box);
         }
     }
+    // CRIA A COMIDA DA COBRINHA.
+    function drawFood() {
+        context.fillStyle = "red";
+        context.fillRect(food.x, food.y, box, box);        
+    }
+
     // DETECTA AS TECLAS DE SETAS.
     document.addEventListener("keydown", update);
     function update(event) {
@@ -44,6 +54,7 @@ let direction = "right"; // DEFINE A DIREÇÃO INICIAL DA COBRINHA.
 
         criarBG();
         criarCobrinha();
+        drawFood();
         // PEGA AS POSIÇÕES | TAMANHO DA COBRINHA.
         let snakeX = snake[0].x;
         let snakeY = snake[0].y;
